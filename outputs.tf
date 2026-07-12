a31f85a8c5f71ce6e82e22f08ac960aefd29605d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "virtual_desktop_scaling_plan_host_pool_associations" {
-  description = "All virtual_desktop_scaling_plan_host_pool_association resources"
-  value       = azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations
-}
 output "virtual_desktop_scaling_plan_host_pool_associations_enabled" {
-  description = "List of enabled values across all virtual_desktop_scaling_plan_host_pool_associations"
-  value       = [for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : v.enabled]
+  description = "Map of enabled values across all virtual_desktop_scaling_plan_host_pool_associations, keyed the same as var.virtual_desktop_scaling_plan_host_pool_associations"
+  value       = { for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : k => v.enabled }
 }
 output "virtual_desktop_scaling_plan_host_pool_associations_host_pool_id" {
-  description = "List of host_pool_id values across all virtual_desktop_scaling_plan_host_pool_associations"
-  value       = [for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : v.host_pool_id]
+  description = "Map of host_pool_id values across all virtual_desktop_scaling_plan_host_pool_associations, keyed the same as var.virtual_desktop_scaling_plan_host_pool_associations"
+  value       = { for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : k => v.host_pool_id }
 }
 output "virtual_desktop_scaling_plan_host_pool_associations_scaling_plan_id" {
-  description = "List of scaling_plan_id values across all virtual_desktop_scaling_plan_host_pool_associations"
-  value       = [for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : v.scaling_plan_id]
+  description = "Map of scaling_plan_id values across all virtual_desktop_scaling_plan_host_pool_associations, keyed the same as var.virtual_desktop_scaling_plan_host_pool_associations"
+  value       = { for k, v in azurerm_virtual_desktop_scaling_plan_host_pool_association.virtual_desktop_scaling_plan_host_pool_associations : k => v.scaling_plan_id }
 }
 
